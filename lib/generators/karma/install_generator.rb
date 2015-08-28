6bd2e0e9356d4a4e99187f3995a2dff391998432
pinglamb <pinglambs@gmail.com>
2015-08-28 15:33:48 +0800

    Use human_name

--- lib/generators/karma/install_generator.rb
+++ lib/generators/karma/install_generator.rb
@@ -1,9 +1,39 @@
 require 'rails/generators'
 
 module Karma
-  class InstallGenerator < ::Rails::Generators::Base
+  class InstallGenerator < ::Rails::Generators::NamedBase
     desc 'Copy Karma Scaffold template files'
     source_root ::File.expand_path('../templates', __FILE__)
+
+    def copy_application_layout
+      remove_file 'app/views/layouts/application.html.erb'
+      template 'layouts/application.html.slim', 'app/views/layouts/application.html.slim'
+    end
+
+    def copy_devise_layout
+      template 'layouts/devise.html.slim', 'app/views/layouts/devise.html.slim'
+    end
+
+    def copy_layout_you
+      copy_file 'layouts/you.html.slim', 'app/views/layouts/shared/_you.html.slim'
+    end
+
+    def keep_components_and_pages
+      add_file 'app/assets/javascripts/components/.gitkeep'
+      add_file 'app/assets/javascripts/pages/.gitkeep'
+      add_file 'app/assets/stylesheets/components/.gitkeep'
+      add_file 'app/assets/stylesheets/pages/.gitkeep'
+    end
+
+    def copy_application_js
+      remove_file 'app/assets/javascripts/application.js'
+      copy_file 'assets/application.js.coffee', 'app/assets/javascripts/application.js.coffee'
+    end
+
+    def copy_application_css
+      remove_file 'app/assets/stylesheets/application.css'
+      copy_file 'assets/application.css.sass', 'app/assets/stylesheets/application.css.sass'
+    end
 
     def copy_views
       copy_file 'views/index.html.slim', 'lib/templates/slim/scaffold/index.html.slim'
